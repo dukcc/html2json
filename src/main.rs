6fa13cec73a08bb8d48c12a97b787daa77c3ec0c
dukcc <dukkcc@proton.me>
2024-08-07 10:27:21 -0700

    cleaned up, 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,17 @@ use std::path::PathBuf;
 struct Args {
     /// The HTML file you want to convert to JSON
     file: PathBuf,
-    /// The output file (optional, will default to the same name, same path)
-    output: PathBuf,
     /// HTML tags that you want to exclude from the JSON
     #[arg(short, long)]
-    exclude: Vec<String>,
+    exclude: Option<Vec<String>>,
 }
 
-fn preprocess_html(html: &str, tags_to_exclude: Vec<String>) -> String {
+fn preprocess_html(html: &str) -> String {
     let mut cleaned_html = html.to_string();
 
     let doctype_pattern = r#"<!DOCTYPE[^>]*>"#;
     let re_doctype = Regex::new(doctype_pattern).unwrap();
     cleaned_html = re_doctype.replace_all(&cleaned_html, "").into_owned();
-
-    for tag in tags_to_exclude {
-        let pattern = format!(r"<{}[^>]*>.*?</{}>", tag, tag);
-        let re = Regex::new(&pattern).unwrap();
-
-        cleaned_html = re.replace_all(&cleaned_html, "").into_owned();
-    }
 
     cleaned_html
 }
@@ -35,14 +26,16 @@ fn preprocess_html(html: &str, tags_to_exclude: Vec<String>) -> String {
 fn main() -> Result<()> {
     let args = Args::parse();
 
+    let output_path = args.file.with_extension("json");
+
     let html_content = std::fs::read_to_string(&args.file)?;
 
-    let cleaned_html = preprocess_html(&html_content, args.exclude);
+    let cleaned_html = preprocess_html(&html_content);
 
     let dom = Dom::parse(&cleaned_html)?;
     let json = dom.to_json_pretty()?;
-    println!("Wrote output to: {:?}", &args.output);
+    println!("Wrote output to: {:?}", &output_path);
 
-    std::fs::write(&args.output, json)?;
+    std::fs::write(&output_path, json)?;
     Ok(())
 }
